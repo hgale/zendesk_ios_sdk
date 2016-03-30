7b81502a2efe5e288056cb20a32495b7d7e30f87
Harry Gale <harry.n.gale@gmail.com>
2016-03-30 10:07:01 -0700

    Further cleanup

---
+++ ZendeskProviderSDK/ZendeskProviderSDK.h
@@ -0,0 +1,19 @@
+//
+//  ZendeskProviderSDK.h
+//  ZendeskProviderSDK
+//
+//  Created by Harry Tormey on 3/30/16.
+//  Copyright © 2016 Harry Tormey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZendeskProviderSDK.
+FOUNDATION_EXPORT double ZendeskProviderSDKVersionNumber;
+
+//! Project version string for ZendeskProviderSDK.
+FOUNDATION_EXPORT const unsigned char ZendeskProviderSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZendeskProviderSDK/PublicHeader.h>
+
+
